d285bf12cda6077da0a2912f06d4c9eb07eea7de
xCallyv <117952316+CallyRoad@users.noreply.github.com>
2023-07-21 11:49:45 +0200

    Add diagonals verification

--- index.js
+++ index.js
@@ -11,18 +11,21 @@ for (let i = 0; i < lines; i++) {
   grid.push(new Array(columns).fill("  "));
 }
 
-//-Fonction pour afficher la grille
+//-Show grid
 function showGrid() {
+  console.log("-".repeat(columns * 3 - 1));
   for (let i = 0; i < lines; i++) {
     console.log(grid[i].join("|"));
     console.log("-".repeat(columns * 3 - 1));
   }
+  console.log(" 0  1  2  3  4  5  6");
+  console.log("");
 }
 
-//-Fonction pour placer un pion dans une colonne choisie
+//-place a pawn in a selected column
 function placePawn(column, player) {
   for (let line = lines - 1; line >= 0; line--) {
-    //Vérifie si la case est vide
+    //Check if the square is empty to place a pawn
     if (grid[line][column] === "  ") {
       grid[line][column] = player;
       return true;
@@ -31,9 +34,9 @@ function placePawn(column, player) {
   console.log("Cette colonne est déjà remplie, essaie une autre !");
   return false;
 }
-//Fonction pour vérifier si un joueur a gagné
+//Check if a player has won
 function verifyVictory(player) {
-  //--On veut vérifier les lignes
+  //--Check lines
   for (let line = 0; line < lines; line++) {
     for (let column = 0; column < columns - 3; column++) {
       if (
@@ -46,7 +49,7 @@ function verifyVictory(player) {
       }
     }
   }
-  //--On veut vérifier les colonnes
+  //--Check columns
   for (let column = 0; column < columns; column++) {
     for (let line = 0; line < lines - 3; line++) {
       if (
@@ -58,63 +61,89 @@ function verifyVictory(player) {
         return true;
       }
     }
+    //--Checking rising diagonals
+    for (let line = 0; line < lines - 3; line++) {
+      for (let column = 0; column < columns - 3; column++) {
+        if (
+          grid[line][column] === player &&
+          grid[line + 1][column + 1] === player &&
+          grid[line + 2][column + 2] === player &&
+          grid[line + 3][column + 3] === player
+        ) {
+          return true;
+        }
+      }
+    }
+    //--Checking downward diagonals
+    for (let line  = 3; line < lines; line++) {
+      for (let column = 0; column < columns - 3; column++) {
+        if (
+          grid[line][column] === player &&
+          grid[line - 1][column + 1] === player &&
+          grid[line - 2][column + 2] === player &&
+          grid[line - 3][column + 3] === player 
+        ) {
+          return true;
+        }
+      }
+    }
   }
   return false;
-  //--On veut vérifier les diagonales
-
 }
-
+//--Starts the game
 function puissance4() {
+  console.log("");
   console.log("-----------------------------------------");
   console.log("Bienvenue sur le jeu du puissance 4 !");
   console.log("-----------------------------------------");
+  console.log("");
 
   let round = 1;
   let currentPlayer = " X ";
 
-  //Fonction qui lance la partie
+  //--Start the rounds
   const gameRound = () => {
     console.log(`Tour : ${round}`);
     showGrid();
 
     rl.question(
-      `C'est au tour du joueur ${currentPlayer}. Choisissez une colonne (0-6) : `,
+      `C'est au tour du joueur ${currentPlayer}. Choisissez une colonne : 0 étant la plus à gauche, et 6 la plus à droite => `,
       (column) => {
         column = parseInt(column);
         if (column >= 0 && column < columns) {
           if (placePawn(column, currentPlayer)) {
             if (verifyVictory(currentPlayer)) {
-              console.log("                             ");
+              console.log("");
               console.log("///////////////////////////");
               console.log(`Le joueur ${currentPlayer} a gagné`);
               console.log("///////////////////////////");
-              console.log("                             ");
+              console.log("");
 
               showGrid();
-              console.log("                             ");
-              console.log("Félicitation ! ");
+              console.log("");
+              console.log(`Félicitation ! Le jeu c'est terminé en ${round} tours ! `);
               rl.close();
               return;
             }
-            //Rajoute un tour
+            //Add round
             round++;
-            //Changement du joueur
+            //Player change
             if (currentPlayer === " X ") {
               currentPlayer = " O ";
             } else {
               currentPlayer = " X ";
             }
-            //Rappel la fonction gameRound() pour pouvoir faire le tour suivant
+            //Call the gameRound() function again to start the next round
             gameRound();
           } else {
             gameRound();
           }
         } else {
-          console.log("                             ");
+          console.log("");
           console.log(
             "---- Entre seulement un chiffre , compris entre 0 et 6 ----"
           );
-          console.log("                             ");
+          console.log("");
           gameRound();
         }
       }
@@ -122,4 +151,6 @@ function puissance4() {
   };
   gameRound();
 }
+
+//Call the puissance4() function for start the game
 puissance4();
